983be1aa03289388a00c1767bf0dd06f13845608
sarah Mwangi <mwangisarah113@gmail.com>
2024-05-29 16:08:28 +0300

    1

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <nav>
+
+  </nav>
+  <router-view/>
+</template>
+
+<style>
+#app {
+  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home">
+<div class="modal"></div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+
+export default {
+  name: 'HomeView',
+  components: {}
+}
+</script>
+
+<style>
+body{
+  background-image: url('../assets/background.jpg');
+  background-size: cover; /* Resize the background image to cover the entire container */
+  height: 500px; /* You must set a specified height */
+  background-position: center; /* Center the image */
+  background-repeat: no-repeat; /* Do not repeat the image */
+
+}
+
+</style>
